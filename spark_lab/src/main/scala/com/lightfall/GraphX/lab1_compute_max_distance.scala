c79bf193204db59c257fcb61378bf6eea261a5fe
Lightfalls <604365090@qq.com>
2020-04-16 10:30:09 +0800

    增加了spark_lab

---
+++ spark_lab/src/main/scala/com/lightfall/GraphX/lab1_compute_max_distance.scala
@@ -0,0 +1,54 @@
+package com.lightfall.GraphX
+
+import org.apache.spark.{SparkConf, SparkContext, graphx}
+import org.apache.spark.graphx.{Edge, EdgeContext, Graph, VertexRDD}
+
+object lab1_compute_max_distance {
+  def sendMsg(ec:EdgeContext[Int,String,Int]):Unit = {
+    ec.sendToDst( ec.srcAttr +1)
+  }
+
+  def mergeMsg(a: Int , b:Int) :Int = {
+    math.max(a,b)
+  }
+
+  def sumEdgeCount( g:Graph[Int,String]):Graph[Int,String] = {
+
+    val record = g.vertices.map(x => x._2).collect.toList
+    val verts = g.aggregateMessages[Int]( sendMsg , mergeMsg)
+
+    val g2 = Graph(verts ,g.edges)
+    val now_record = g2.vertices.map(x => x._2).collect.toList
+
+    val check = now_record == record
+
+    if(!check)
+      sumEdgeCount(g2)
+    else
+      g
+  }
+
+  def main(args: Array[String]): Unit = {
+    //设置运行环境
+    val conf = new SparkConf().setAppName("SimpleGraphX").setMaster("local")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    // 构建图
+    val myVertices = sc.parallelize(Array((1L, "张三"), (2L, "李四"), (3L, "王五"), (4L, "钱六"),
+      (5L, "领导")))
+    val myEdges = sc.makeRDD(Array( Edge(1L,2L,"朋友"),
+      Edge(2L,3L,"朋友") , Edge(3L,4L,"朋友"),
+      Edge(4L,5L,"上下级"),Edge(3L,5L,"上下级")
+    ))
+
+    val myGraph = Graph(myVertices,myEdges)
+
+    val initGraph = myGraph.mapVertices((_,_)=>0)
+
+    println("result------------------------------------------------")
+    sumEdgeCount(initGraph).vertices.collect.foreach(println(_))
+    println("------------------------------------------------------")
+  }
+
+}
